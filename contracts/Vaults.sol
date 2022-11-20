cdabb04d51f5318888d15f24da24da59e0daf74f
harpoonjs <w3st.2021@gmail.com>
2022-11-20 17:24:04 -0500

    Improved

--- contracts/Vaults.sol
+++ contracts/Vaults.sol
@@ -6,14 +6,13 @@ pragma solidity ^0.8.10;
 /*** [IMPORT] ***/
 
 contract Vaults {	
-	/*** [STATE VARIABLES] ***/
+	/*** [STRUCTS] ***/
 	
 	struct Vault {
 		mapping(address => uint8) voterWeight;
 		address[] acceptedTokens;
 		uint256 withdrawMinutesDelay;
 	}
-
 
 	struct withdrawalRequest {
 		address requester;
