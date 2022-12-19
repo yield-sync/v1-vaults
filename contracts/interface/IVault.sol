9846158096674edac7c12ee3c3edf99a8089dab8
harpoonjs <w3st.io2021@gmail.com>
2022-12-19 10:55:29 -0500

    Writing out the interface contract

--- contracts/interface/IVault.sol
+++ contracts/interface/IVault.sol
@@ -8,24 +8,45 @@ pragma solidity ^0.8.1;
 */
 interface IVault
 {
+	/* [STRUCT] */
+	struct WithdrawalRequest {
+		address creator;
+		address to;
+		address token;
+		bool paused;
+		bool accelerated;
+		uint256 amount;
+		uint256 forVoteCount;
+		uint256 againstVoteCount;
+		uint256 lastImpactfulVote;
+	}
+
 	/**
+	* @dev [bytes32]
 	* @notice AccessControl Role
 	* @return {bytes32} keccak256 value
 	*/
 	function VOTER_ROLE() external view returns (bytes32);
 
 	/**
+	* @dev [uint256]
 	* @notice Required signatures for a vote
 	* @return {bytes32} keccak256 value
 	*/
 	function requiredSignatures() external view returns (uint256);
 
 	/**
+	* @dev [uint256]
 	* @notice Delay in minutes withdrawal
 	* @return {bytes32} keccak256 value
 	*/
 	function withdrawalDelayMinutes() external view returns (uint256);
 
-
-	mapping(address => uint256) public tokenBalance;
+	/**
+	* @dev [mapping]
+	* @notice Token Balances
+	* @param {address} Token contract address
+	* @return {uint256} Balance of token
+	*/
+	function tokenBalance(address token) external view returns (uint256);
 }
