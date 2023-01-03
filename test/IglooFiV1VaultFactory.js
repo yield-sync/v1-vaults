554eddf7905f54d6f60e2162b334b5e3f61360e0
harpoonjs <w3st.io2021@gmail.com>
2023-01-02 19:53:38 -0500

    Writing out first part of tests

--- test/IglooFiV1VaultFactory.js
+++ test/IglooFiV1VaultFactory.js
@@ -1,17 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
+/* [variables] */
+const iglooFiGovernanceAddress = ethers.utils.getAddress("0x96A4BC19E6947b8C4b3FbC47bAE0dCB32F0037c8")
+
+
 describe("IglooFiV1VaultFactory", function () {
 	it(
-		"Should..",
+		"Should set IGLOO_FI to iglooFi",
 		async function () {
-			const IglooFiV1VaultFactory = await ethers.getContractFactory(
-				"IglooFiV1VaultFactory"
-			);
-
-			const r_IglooFiV1VaultFactory = await IglooFiV1VaultFactory.deploy("");
+			const IglooFiV1VaultFactory = await ethers.getContractFactory("IglooFiV1VaultFactory");
+			const iglooFiV1VaultFactory = await IglooFiV1VaultFactory.deploy(iglooFiGovernanceAddress);
 		
-			await r_IglooFiV1VaultFactory.deployed()
+			const contract = await iglooFiV1VaultFactory.deployed();
+
+			const IGLOO_FI = await contract.IGLOO_FI();
+
+			expect(IGLOO_FI).to.equal(iglooFiGovernanceAddress);
+		}
+	);
+
+	it(
+		"Should set fee to 0 in constructor",
+		async function () {
+			const IglooFiV1VaultFactory = await ethers.getContractFactory("IglooFiV1VaultFactory");
+			const iglooFiV1VaultFactory = await IglooFiV1VaultFactory.deploy(iglooFiGovernanceAddress);
+		
+			const contract = await iglooFiV1VaultFactory.deployed();
+
+			const fee = await contract.fee();
+
+			expect(fee).to.equal(0);
 		}
 	);
 });
