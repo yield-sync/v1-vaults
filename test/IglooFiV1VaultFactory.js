8f804a5907b58f2316f225289ffaf9c2ed481154
harpoonjs <w3st.io2021@gmail.com>
2023-01-02 20:57:11 -0500

    Improved

--- test/IglooFiV1VaultFactory.js
+++ test/IglooFiV1VaultFactory.js
@@ -8,6 +8,15 @@ const vitalik = ethers.utils.getAddress("0xd8da6bf26964af9d7eed9e03e53415d37aa96
 
 
 describe("IglooFiV1VaultFactory", function () {
+	// Log the network
+	console.log("Testing on Network:", network.name);
+
+	
+	before('create fixture loader', async () => {
+		[vitalik] = await (ethers).getSigners();
+	})
+
+
 	it(
 		"Should set IGLOO_FI to iglooFi",
 		async function () {
@@ -29,4 +38,16 @@ describe("IglooFiV1VaultFactory", function () {
 			expect(await contract.fee()).to.equal(0);
 		}
 	);
+
+	it(
+		"Should update fee correctly",
+		async function () {
+			const IglooFiV1VaultFactory = await ethers.getContractFactory("IglooFiV1VaultFactory");
+			const iglooFiV1VaultFactory = await IglooFiV1VaultFactory.deploy(iglooFiGovernanceAddress);
+			const contract = await iglooFiV1VaultFactory.deployed();
+
+			expect(await contract.fee()).to.equal(1);
+		}
+	);
+
 });
