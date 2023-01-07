46b6ae8d2de3aaa66b21b3d5a08a1dce25dc7c50
harpoonjs <w3st.io2021@gmail.com>
2023-01-07 09:55:53 -0500

    Improve test

--- test/IglooFiV1VaultFactory.js
+++ test/IglooFiV1VaultFactory.js
@@ -4,18 +4,14 @@ const { ethers } = require("hardhat");
 
 /* [variables] */
 const iglooFiGovernanceAddress = ethers.utils.getAddress("0x0000000000000000000000000000000000000000");
-const vitalik = ethers.utils.getAddress("0xd8da6bf26964af9d7eed9e03e53415d37aa96045");
 
 
 describe("IglooFiV1VaultFactory", function () {
-	// Log the network
-	console.log("Testing on Network:", network.name);
-
-	
-	before('create fixture loader', async () => {
-		//[vitalik] = await (ethers).getSigners();
+	before('', async () => {
+		// Log the network
+		console.log("Testing on Network:", network.name);
 	})
-
+	
 
 	it(
 		"Should set IGLOO_FI to iglooFi",
