14c5fd210a9f10bf2dd7fbe49ab4583babef70da
harpoonjs <w3st.io2021@gmail.com>
2023-02-10 15:50:59 -0500

    Broken

--- test/A_VerifySignature.ts
+++ test/A_VerifySignature.ts
@@ -52,7 +52,7 @@ describe("Verify Signature", async () => {
 				await contract.deployed();
 
 				
-				const domainSeparator = ethers.utils.id("EIP-712");
+				const domainSeparator = contract.getMessageHash("EIP-712");
 				const hash = await contract.getMessageHash("Hello, world!");
 				
 				const typedDataHash = await contract.ECDSA_toTypedDataHash(domainSeparator, hash);
@@ -64,6 +64,30 @@ describe("Verify Signature", async () => {
 				expect(
 					await contract.ECDSA_recover(typedDataHash, signature)
 				).to.equal(owner.address);
+
+			});
+
+			it("domain separator returns properly", async () => {
+				// Get contract
+				const VerifySignature = await ethers.getContractFactory("VerifySignature");
+
+				// Deploy contract
+				const contract = await VerifySignature.deploy();
+				await contract.deployed();
+				
+				
+				const chainId = 1; //the `chainId` of the chain the contract is deployed on_
+				const _name = 1; //the `name()` of the ERC20 contract_;
+				const version = 1; //the version as specified in ERC712 (string, generally of a number)_;
+				const verifyingContract = 1; //address of the contract the function is in_;
+
+				expect(await contract.DOMAIN_SEPARATOR())
+					.to.equal(await ethers.utils._TypedDataEncoder.hashDomain({
+					name: _name,
+					version,
+					chainId,
+					verifyingContract
+				}));
 			});
 		});
 	});
