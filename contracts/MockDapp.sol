cd4443e416a2910f93ef322f2fb5b7d8c74e08b0
harpoonjs <w3st.io2021@gmail.com>
2023-02-24 11:46:34 -0500

    Make linter more strict

--- contracts/MockDapp.sol
+++ contracts/MockDapp.sol
@@ -5,22 +5,29 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 
+interface IMockDapp {
+	function getDomainSeperator() external view returns (bytes32);
+	function getStructHash(address player, uint points) external pure returns (bytes32);
+}
+
+
 /**
  * @title MockDapp
 */
 contract MockDapp is
-	EIP712
+	EIP712,
+	IMockDapp
 {
 	constructor ()
 		EIP712("MockDapp", "1")
 	{}
 
 
-	function getDomainSeperator() public view returns (bytes32) {
+	function getDomainSeperator() public view override returns (bytes32) {
 		return _domainSeparatorV4();
 	}
 
-	function getStructHash(address player, uint points) public pure returns (bytes32) {
+	function getStructHash(address player, uint points) public pure override returns (bytes32) {
 		return keccak256(abi.encode(keccak256("Score(address player,uint points)"), player, points));
 	}
 }
