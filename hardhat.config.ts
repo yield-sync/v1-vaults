76664f68e74ab5961b5ded9ea696286fe8165442
harpoonjs <w3st.io2021@gmail.com>
2023-02-24 11:47:47 -0500

    Format hardhat.config.ts file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,32 +7,32 @@ import "@nomiclabs/hardhat-solhint";
 
 /**
  * @type import("hardhat/config").HardhatUserConfig
- */
+*/
 const config: any = {
-  solidity: "0.8.10",
-  networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    },
-    optimism: {
-      url: `https://mainnet.optimism.io`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    },
-  },
-  etherscan: {
-    mainnet: process.env.ETHERSCAN_API_KEY,
-    goerli: process.env.ETHERSCAN_API_KEY,
-    optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY,
-  }
+	solidity: "0.8.10",
+	networks: {
+		goerli: {
+			url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+			accounts: [`0x${process.env.PRIVATE_KEY}`]
+		},
+		ropsten: {
+			url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+			accounts: [`0x${process.env.PRIVATE_KEY}`]
+		},
+		mainnet: {
+			url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+			accounts: [`0x${process.env.PRIVATE_KEY}`]
+		},
+		optimism: {
+			url: `https://mainnet.optimism.io`,
+			accounts: [`0x${process.env.PRIVATE_KEY}`]
+		},
+	},
+	etherscan: {
+		mainnet: process.env.ETHERSCAN_API_KEY,
+		goerli: process.env.ETHERSCAN_API_KEY,
+		optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY,
+	}
 };
 
 
