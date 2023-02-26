a1b2f8f4f590c44addf2dc68731b831b7edaf6ce
harpoonjs <w3st.io2021@gmail.com>
2023-02-26 16:16:09 -0500

    Replace spaces with tabs

--- contracts/MockERC20.sol
+++ contracts/MockERC20.sol
@@ -6,7 +6,7 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock ERC20", "MOCK") {
-        _mint(msg.sender, 100_000_000_000);
-    }
+	constructor() ERC20("Mock ERC20", "MOCK") {
+		_mint(msg.sender, 100_000_000_000);
+	}
 }
