442434980bcb39d700faad833dc016cb09333724
harpoonjs <w3st.io2021@gmail.com>
2023-02-26 15:52:29 -0500

    Comply to linter

--- contracts/MockIglooFiGovernance.sol
+++ contracts/MockIglooFiGovernance.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 
-import "@igloo-fi/v1-sdk/contracts/IglooFiGovernance.sol";
+import { IglooFiGovernance } from "@igloo-fi/v1-sdk/contracts/IglooFiGovernance.sol";
 
 
 contract MockIglooFiGovernance is IglooFiGovernance {}
