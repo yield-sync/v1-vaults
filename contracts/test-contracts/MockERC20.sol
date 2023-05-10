72f4d573955e30d135c8c9174bc8abf36f417359
harpoon js <w3st.io2021@gmail.com>
2023-05-10 15:05:58 -0400

    Increase amount of tokens that msg.sender gets upon token launch

--- contracts/test-contracts/MockERC20.sol
+++ contracts/test-contracts/MockERC20.sol
@@ -7,6 +7,7 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
 	constructor() ERC20("MockERC20", "MOCKERC20") {
-		_mint(msg.sender, 100_000_000_000);
+		// Sent msg.sender 100m MOCKERC20 tokens
+		_mint(msg.sender, 100_000_000_000_000_000_000_000_000);
 	}
 }
