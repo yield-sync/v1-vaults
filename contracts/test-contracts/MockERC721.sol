45a2b63bddf2c37ff2d8f85f9f6c1af6b1a319cc
harpoon js <w3st.io2021@gmail.com>
2023-06-25 21:02:20 -0400

    Better nft token test contract

--- contracts/test-contracts/MockERC721.sol
+++ contracts/test-contracts/MockERC721.sol
@@ -6,11 +6,19 @@ import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract MockERC721 is ERC721 {
-	constructor() ERC721("MockERC721", "MOCKERC721") {
+	constructor()
+		ERC721("MockERC721", "MOCKERC721")
+	{
 		_safeMint(msg.sender, 1);
 		_safeMint(msg.sender, 2);
 		_safeMint(msg.sender, 3);
 		_safeMint(msg.sender, 4);
 		_safeMint(msg.sender, 5);
+	}
+
+	function safeMint(uint256 tokenId)
+		public
+	{
+		_safeMint(msg.sender, tokenId);
 	}
 }
