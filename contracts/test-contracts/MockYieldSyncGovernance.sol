70fdcc335e51c40dcae7675917b5a76e39d13ced
harpoon js <w3st.io2021@gmail.com>
2023-08-06 21:21:09 -0400

    Formatted mock contracts

--- contracts/test-contracts/MockYieldSyncGovernance.sol
+++ contracts/test-contracts/MockYieldSyncGovernance.sol
@@ -5,5 +5,6 @@ pragma solidity ^0.8.18;
 import { YieldSyncGovernance } from "@yield-sync/v1-sdk/contracts/YieldSyncGovernance.sol";
 
 
-contract MockYieldSyncGovernance is YieldSyncGovernance
+contract MockYieldSyncGovernance is
+	YieldSyncGovernance
 {}
