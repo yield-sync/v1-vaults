2aabd05662409c89e0028547b1b16bc26f4ad51c
harppon <aleem.ahmed2015@gmail.com>
2023-08-12 01:01:24 -0400

    Typing and formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,8 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-solhint";
 
 require("dotenv").config();
-require('@nomiclabs/hardhat-ethers');
-require('@eth-optimism/plugins/hardhat/compiler');
+require("@nomiclabs/hardhat-ethers");
+require("@eth-optimism/plugins/hardhat/compiler");
 
 
 export default {
@@ -21,49 +21,46 @@ export default {
 	networks: {
 		goerli: {
 			url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`]
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
 		},
 		ropsten: {
 			url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`]
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
 		},
 		mainnet: {
 			url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`]
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
 		},
 		optimism: {
 			url: `https://mainnet.optimism.io`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`],
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
 			gasPrice: 15000000,
 			ovm: true
 		},
 		optimismgoerli: {
 			url: `https://goerli.optimism.io`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`],
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
 			gasPrice: 15000000
 		},
 		sepolia: {
 			url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}`]
+			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
 		},
-		// for mainnet
-		'base-mainnet': {
-			url: 'https://mainnet.base.org',
+		"base-mainnet": {
+			url: "https://mainnet.base.org",
 			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
 			gasPrice: 1000000000,
-		  },
-		  // for testnet
-		  'base-goerli': {
-			url: 'https://goerli.base.org',
+		},
+		"base-goerli": {
+			url: "https://goerli.base.org",
 			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
 			gasPrice: 1000000000,
-		  },
-		  // for local dev environment
-		  'base-local': {
-			url: 'http://localhost:8545',
+		},
+		"base-local": {
+			url: "http://localhost:8545",
 			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
 			gasPrice: 1000000000,
-		  },
+		},
 	},
 	paths: {
 		sources: "./contracts",
